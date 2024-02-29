83c0f0d8926a29f6e66256bbbf1d62af0d76032a
roger2b <rmvd2@cam.ac.uk>
2024-02-29 10:01:22 -0800

    updated kernel installation guide +

--- create_jax_env.sh
+++ create_jax_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Create a JAX environment at NERSC.
-#
+# date: 02292024
 # Need to module load these in the scripts that are calling
 # code running under this environment as well.
 #
@@ -13,16 +13,16 @@ module load python
 #
 # Set up the environment, cloning from nersc-mpi4py to get the
 # proper MPI environment.
-conda create --name jax-env --clone nersc-mpi4py
-conda activate jax-env
+conda create --name jax-env-gpu --clone nersc-mpi4py
+conda activate jax-env-gpu
 conda update --all -y
 conda install -c conda-forge numpy scipy ipykernel -y
 python3 -Xfrozen_modules=off -m ipykernel \
-        install --user --name jax-env --display-name JAX-env
+        install --user --name jax-env-gpu --display-name JAX-env-gpu
 #
 conda install -c conda-forge numba healpy camb -y
 #
-python3 -m pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+python3 -m pip install --upgrade "jax[cuda12_pip]==0.4.23" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 #
 #
 # test installation
@@ -30,3 +30,26 @@ python3 -m pip install --upgrade "jax[cuda12_pip]" -f https://storage.googleapis
 #
 python3 -m pip install -v git+https://github.com/martinjameswhite/directsht
 #
+# For testing purposes:
+# python
+# import jax
+# jax.config.update("jax_enable_x64", True)
+# from jax.lib import xla_bridge
+# print(xla_bridge.get_backend().platform)
+
+# try:
+#     jax_present = True
+#     from jax import vmap, jit, devices
+#     import jax.numpy as jnp
+#     import sht.legendre_jax as legendre
+#     import sht.interp_funcs_jax as interp
+#     import sht.utils_jax as utils
+#     from   sht.utils_jax import move_to_device
+# except ImportError:
+#     jax_present = False
+#     move_to_device = lambda x, **kwargs: x  # Dummy definition for fallback
+#     print("JAX not found. Falling back to NumPy.")
+#     from numba import njit as jit
+#     import sht.legendre_py as legendre
+#     import sht.interp_funcs_py as interp
+#     import sht.utils_py as utils
--- notebooks/matplotlib_params_file.py
+++ notebooks/matplotlib_params_file.py
@@ -1,4 +1,12 @@
 import matplotlib.pyplot as plt
+# make plots prettier
+from matplotlib.pyplot import rc
+import matplotlib.font_manager
+
+rc('font',**{'size':'22','family':'serif','serif':['CMU serif']})
+rc('mathtext', **{'fontset':'cm'})
+rc('text', usetex=True)
+rc('legend',**{'fontsize':'18'})
 
 plt.rcParams['text.usetex'] = False
 plt.rcParams['font.family'] = 'serif'
@@ -23,3 +31,20 @@ plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
 plt.rcParams['xtick.major.pad'] = '8'
 plt.rcParams['figure.figsize'] = 12,8
+
+
+plt.rcParams['axes.linewidth'] = 3
+plt.rcParams['axes.labelsize'] = 30
+plt.rcParams['xtick.labelsize'] = 25 
+plt.rcParams['ytick.labelsize'] = 25
+plt.rcParams['legend.fontsize'] = 25
+plt.rcParams['xtick.major.size'] = 10
+plt.rcParams['ytick.major.size'] = 10
+plt.rcParams['xtick.minor.size'] = 5
+plt.rcParams['ytick.minor.size'] = 5
+plt.rcParams['xtick.major.width'] = 3
+plt.rcParams['ytick.major.width'] = 3
+plt.rcParams['xtick.minor.width'] = 1.5
+plt.rcParams['ytick.minor.width'] = 1.5
+plt.rcParams['axes.titlesize'] = 30
+plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
