f18f42d7bfaaca9be33a37a8643342ba14cacc54
Roger de Belsunce <r.belsunce@gmail.com>
2024-06-05 12:28:14 +0200

    Cell SHT lya periodic as mat mul

--- notebooks/SHT_lya.py
+++ notebooks/SHT_lya.py
@@ -24,6 +24,15 @@ def compute_dft(chi_grid, mask, delta):
     FT_delta = np.dot(delta, FT_mat)
     return k_arr, np.real(FT_mask), np.real(FT_delta)
 
+def DFT_slow(x):
+    """Compute the discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
 #################### theory module functions ####################
 
 def compute_nhat(tdata, pdata):
